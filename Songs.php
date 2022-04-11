bdf7278d26b07f5f727059bbbf38ef5f4747904a
PantojaGerson <gersonpantoja22.gp@gmail.com>
2022-04-11 08:23:04 +0800

    initial files has been added

---
+++ Songs.php
@@ -0,0 +1,323 @@
+                        <!-- Song_1: Habang Buhay -->
+                        <?php
+
+                            echo "<p> Aking sinta </p>";
+                            echo "<p> Ano bang meron sa iyo </p>";
+                            echo "<p> Pag nakikita ka na </p>";
+                            echo "<p> Bumabagal ang mundo </p>";
+                            echo "<br/>";
+
+                            echo "<p> Pag ngumingiti ka </p>";
+                            echo "<p> Para bang may iba </p>";
+                            echo "<p> Pag tumitingin sakin </p>";
+                            echo "<p> Mapupungay mong mga mata </p>";
+                            echo "<p> Wala akong takas sa nakakalunod mong ganda </p>";
+                            echo "<p> Halika na </p>";
+                            echo "<br/>";
+
+                            echo "<p> Tingnan mo lang ang aking mga mata </p>";
+                            echo "<p> Wag kang titingin pa sa iba </p>";
+                            echo "<p> Akin ka na </p>";
+                            echo "<p> Wala ng iba </p>";
+                            echo "<br/>";
+
+                            echo "<p> Andito ka hanggang saating pagtanda</p>";
+                            echo "<p> Mamahalin kita basta't pag nahulog </p>";
+                            echo "<p> Naka hawak ako </p>";
+                            echo "<p> Wag ka lang bibitaw </p>";
+                            echo "<p> Habang buhay na ako'y iyo </p>";
+                            echo "<br/>";
+
+                            echo "<p> Wala na ibang </p>";
+                            echo "<p> Nakagawa sakin ng ganto </p>";
+                            echo "<p> Kundi ikaw </p>";
+                            echo "<p> Nagiisang diyosa ng buhay ko </p>";
+                            echo "<br/>";
+
+                            echo "<p> Wag ka ng matakot </p>";
+                            echo "<p> Wag kang mangamba </p>";
+                            echo "<p> Andito ako pag ikay magisa </p>";
+                            echo "<p> Wala akong takas sa nakakalunod mong ganda</p>";
+                            echo "<p> Halika nga </p>";
+                            echo "<br/>";
+
+                            echo "<p> Tingnan mo lang ang aking mga mata </p>";
+                            echo "<p> Wag kang titingin na sa iba </p>";
+                            echo "<p> Akin ka na </p>";
+                            echo "<p> Wala ng iba </p>";
+                            echo "<br/>";
+
+                            echo "<p> Andito ako hanggang saating pagtanda </p>";
+                            echo "<p> Mamahalin kita basta't pag nahulog </p>";
+                            echo "<p> Naka hawak ako </p>";
+                            echo "<p> Wag ka lang bibitaw </p>";
+                            echo "<p> Habang buhay na ako'y iyo </p>";
+                            echo "<br/>";
+
+                            echo "<p> Kahit ang likod mo'y kubang kuba na </p>";
+                            echo "<p> Kahit ang ulo mo'y puro uban na </p>";
+                            echo "<p> Isasayaw ka hanggang sa pikit na ang ating mga mata </p>";
+                            echo "<br/>";
+
+                            echo "<p> Wala naman na akong hiling pa </p>";
+                            echo "<p> Bastat kasama ka habang buhay na  </p>";
+                            echo "<p> Kuntento ako bastat ikaw lang kasama </p>";
+                            echo "<p> Ikaw kasama ko </p>";
+                            echo "<br/>";
+
+                            echo "<p> Aking sinta </p>";
+                            echo "<p> Ano bang meron sa iyo </p>";
+                            echo "<p> Pag nakikita ka na </p>";
+                            echo "<p> Bumabagal ang mundo  </p>";
+                            echo "<br/>";
+
+                            echo "<p> Tingnan mo lang ang aking mata </p>";
+                            echo "<p> Wag kang titingin pa sa iba </p>";
+                            echo "<p> Akin ka na </p>";
+                            echo "<p> Wala ng iba </p>";
+                            echo "<br/>";
+
+                            echo "<p> Andito ko hanggang saating pagtanda </p>";
+                            echo "<p> Mamahalin kita basta't pag nahulog </p>";
+                            echo "<p> Nakahawak ako </p>";
+                            echo "<p> Wag ka lang bibitaw </p>";
+                            echo "<p> Habang buhay na ako'y iyo </p>";
+                            echo "<br/>";
+
+                            echo "<p> Tingnan mo lang ang aking mata </p>";
+                            echo "<p> Wag kang titingin pa sa iba </p>";
+                            echo "<p> Akin ka na </p>";
+                            echo "<p> Wala ng iba </p>";
+                            echo "<br/>";
+
+                        ?>
+                         <!-- Song_2: Anyone by Justin Bieber -->
+                        <?php
+
+                            echo "<p> Dance with me under the diamonds </p>";
+                            echo "<p> See me like breath in the cold </p>";
+                            echo "<p> Sleep with me here in the silence </p>";
+                            echo "<p> Come kiss me, silver and gold </p>";
+                            echo "<br/>";
+
+                            echo "<p> You say that I won't lose you, but you
+                                    can't predict the future </p>";
+                            echo "<p> So just hold on like you will never let go </p>";
+                            echo "<p> Yeah, if you ever move on without me </p>";
+                            echo "<p> I need to make sure you know that </p>";
+                            echo "<br/>";
+
+                            echo "<p> You are the only one I'll ever love
+                                    (I gotta tell ya, gotta tell ya) </p>";
+                            echo "<p> Yeah, you, if it's not you, it's not anyone
+                                    (I gotta tell ya, gotta tell ya) </p>";
+                            echo "<p>  Lookin' back on my life, you're the only good I've ever done
+                                    (Ever done) </p>";
+                            echo "<p> Yeah, you, if it's not you, it's not anyone (Anyone), not anyone </p>";
+                            echo "<br/>";
+
+                            echo "<p> Forever's not enough time to (Oh) </p>";
+                            echo "<p> Love you the way that I want (Love you the way that I want) </p>";
+                            echo "<p> Cause' every mornin', I'll find you (Oh) </p>";
+                            echo "<p> I fear the day that I don't </p>";
+                            echo "<br/>";
+
+                             echo "<p> You say that I won't lose you, but you
+                                    can't predict the future </p>";
+                            echo "<p> Cause' certain things are out of our control </p>";
+                            echo "<p> Yeah, if you ever move on without me </p>";
+                            echo "<p> I need to make sure you know that </p>";
+                            echo "<br/>";
+
+                            echo "<p> You are the only one I'll ever love
+                                    (I gotta tell ya, gotta tell ya) </p>";
+                            echo "<p> Yeah, you, if it's not you, it's not anyone
+                                    (I gotta tell ya, gotta tell ya) </p>";
+                            echo "<p>  Lookin' back on my life, you're the only good I've ever done
+                                    (I've ever done) </p>";
+                            echo "<p> Yeah, you, if it's not you, it's not anyone (It's not anyone, not anyone </p>";
+                            echo "<br/>";
+
+                            echo "<p> Oh-oh-oh-oh </p>";
+                            echo "<p> If it's not you, it's not anyone </p>";
+                            echo "<p> oh-oh-oh, yeah, woah</p>";
+                            echo "<br/>";
+
+                            echo "<p> Yeah, you are the only one I'll ever love (I gotta tell ya, gotta tell ya, yeah, oh-oh-oh-oh) </p>";
+                            echo "<p> Yeah, you, if it's not you, it's not anyone (I gotta tell ya, gotta tell you </p>";
+                            echo "<p> Lookin' back on my life, you're the only good I've ever done (Ever done, oh yeah) </p>";
+                            echo "<p> Yeah, you, if it's not you, it's not anyone </p>";
+
+                        ?>
+
+                        <!-- Song_3: How deep is your love by Bee Gees -->
+                        <?php
+                            echo "<p> I know your eyes in the morning sun </p>";
+                            echo "<p> I feel you touch me in the pouring </p>";
+                            echo "<p> And the moment that you wander far from me </p>";
+                            echo "<p> I wanna feel you in my arms again </p>";
+                            echo "<br/>";
+
+                            echo "<p> And you come to me on a summer breeze </p>";
+                            echo "<p> Keep me warm in your love then you softly breeze </p>";
+                            echo "<p> And it's me you need to show </p>";
+                            echo "<p> How deep is your love </p>";
+                            echo "<br/>";
+
+                            echo "<p> How deep is your love </p>";
+                            echo "<p> How deep is your love </p>";
+                            echo "<p> I really mean to learn </p>";
+                            echo "<p> 'Cause we're living in the world of fools </p>";
+                            echo "<p> Breaking us down </p>";
+                            echo "<p> When they all should let us be </p>";
+                            echo "<p> We belong to you and me </p>";
+                            echo "<br/>";
+
+                            echo "<p> I believe in you </p>";
+                            echo "<p> You know the door to my very soul </p>";
+                            echo "<p> You're the light in my deepest darkest hour </p>";
+                            echo "<p> You're my saviour when I fall </p>";
+                            echo "<p> And you may not think </p>";
+                            echo "<p> I care for you </p>";
+                            echo "<p> When you know down inside </p>";
+                            echo "<p> That I really do </p>";
+                            echo "<p> And it's me you need to show </p>";
+                            echo "<p> How deep is your love </p>";
+                            echo "<br/>";
+
+                            echo "<p> How deep is your love </p>";
+                            echo "<p> How deep is your love </p>";
+                            echo "<p> I really mean to learn </p>";
+                            echo "<p> 'Cause we're living in the world of fools </p>";
+                            echo "<p> Breaking us down </p>";
+                            echo "<p> When they all should let us be </p>";
+                            echo "<p> We belong to you and me </p>";
+                            echo "<br/>";
+
+                            echo "<p> And you come to me on a summer breeze </p>";
+                            echo "<p> Keep me warm in your love then you softly leave </p>";
+                            echo "<p> And it's me you need to know </p>";
+                            echo "<p>  How deep is your love </p>";
+                            echo "<br/>";
+
+                            echo "<p> How deep is your love </p>";
+                            echo "<p> How deep is your love </p>";
+                            echo "<p> I really mean to learn </p>";
+                            echo "<p> 'Cause we're living in the world of fools </p>";
+                            echo "<p> Breaking us down </p>";
+                            echo "<p> When they all should let us be </p>";
+                            echo "<p> We belong to you and me </p>";
+                            echo "<p> [Repeat fading out]</p>"
+                        ?>
+
+                        <!-- Song_4: Manila by Young Cocoa -->
+                        <?php
+
+                            echo "<p> Yeah, uh, woah, yeah, ayy </p>";
+                            echo "<br/>"
+
+                            echo "<p> Urban Jungle, steppin' on a puddle </p>";
+                            echo "<p> Mind race ejaculate the thoughts that's in the bubble </p>";
+                            echo "<p> Workin' for the better 'morrow, workin' for the payday</p>";
+                            echo "<p> Rememberin' the things that's in the past that's in the heyday</p>";
+                            echo "<p> Way way, back back, when when, time right </p>";
+                            echo "<p> Sneakers so white white, comin' through the city </p>";
+                            echo "<br/>";
+
+                            echo "<p> Comin' out just for night night </p>";
+                            echo "<p> Vios got modded and all </p>";
+                            echo "<p> And I'm whippin' out my paper on my favorite stall </p>";
+                            echo "<p> Mang nestor, feelin' like a jester </p>";
+                            echo "<p> Swervin' so heavy alcohol go up on tester </p>";
+                            echo "<p> Pourin' out a bottle just to show respect for ones that I love </p>";
+                            echo "<p> It's about a little time I show Manila some love </p>";
+                            echo "<br/>";
+
+                            echo "<p> Manila (you know, you know, you know) yeah, uh, woah, yeah, ayy</p>";
+                            echo "<p> Manila (you know, you know, you know) yeah, woah,  ayy, yeah</p>";
+                            echo "<p> Manila (you know, you know, you know) ayy, yeah, woah";
+                            echo "<p> (You know, you know, you know) </p>";
+
+                            echo "<p> Got a couple hundred in my pocket tryna' to see ya </p>";
+                            echo "<p> Blowin' out my stash, bouta see if I can re-up </p>";
+                            echo "<p> Pullin' up in Katip bouta see if I can see homie </p>";
+                            echo "<p> Got a girl from Alabang and she gon' be on me </p>";
+                            echo "<p> Pullin' up in EDSA, swerve down the skyway </p>";
+                            echo "<p> Ain't no bad vibes 2020 this is my way </p>";
+                            echo "<p> My way or the highway </p>";
+                            echo "<br/>";
+
+                            echo "<p> Pull up in the Angkas </p>";
+                            echo "<p> Don't forget to (skrr skrr) </p>";
+                            echo "<p> I don't fuck with Joyride's 'cause they don't respect the worker </p>";
+                            echo "<p> Diokno in the biker seat got him with a mask and a globe </p>";
+                            echo "<p> And I exercise my right to pay respect to Above </p>";
+                            echo "<p> Filipino kid, just a mix of cultural things </p>";
+                            echo "<p> Showin' love to Manila it's city of dreams </p>";
+                            echo "<br/>";
+
+                            echo "<p> Yeah, slide through with a couple of friends </p>";
+                            echo "<p> Jammin' out so heavy got a chos in my hands </p>";
+                            echo "<p> Grew up in the city call J-K-T </p>";
+                            echo "<p> With the tempe, sauce and a couple of beats </p>";
+                            echo "<p> Flew back to Manila in search of my own </p>";
+                            echo "<p> Pay respect to the city that became my home </p>";
+                            echo "<br/>";
+
+                            echo "<p> Manila (you know, you know, you know) ayy, yeah, woah, yeah </p>";
+                            echo "<p> Manila (you know, you know, you know) uh, yeah, woah, ayy, yeah</p>";
+                            echo "<p> Manila (you know, you know, you know) ayy, yeah, woah";
+                            echo "<p> (You know, you know, you know) </p>";
+
+                            echo "<p> Got a couple hundred in my pocket tryna' to see ya </p>";
+                            echo "<p> Blowin' out my stash, bouta see if I can re-up </p>";
+                            echo "<p> Pullin' up in Katip bouta see if I can see homie </p>";
+                            echo "<p> Got a girl from Alabang and she gon' be on me </p>";
+                            echo "<p> Pullin' up in EDSA, swerve down the skyway </p>";
+                            echo "<p> Ain't no bad vibes 2020 this is my way </p>";
+                            echo "<p> My way or the highway </p>";
+                            echo "<br/>";
+
+                            echo "<p> Got a couple hundred in my pocket tryna' to see ya </p>";
+                            echo "<p> Blowin' out my stash, bouta see if I can re-up </p>";
+                            echo "<p> Pullin' up in Katip bouta see if I can see homie </p>";
+                            echo "<p> Got a girl from Alabang and she gon' be on me </p>";
+                            echo "<p> Pullin' up in EDSA, swerve down the skyway </p>";
+                            echo "<p> Ain't no bad vibes 2020 this is my way </p>";
+                            echo "<p> My way or the highway </p>";
+                            echo "<br/>";
+
+                        ?>
+
+                        <!-- Song_5: The night we met by Lord Huron -->
+                        <?php
+
+                            echo "<p> I am not the only traveler </p>";
+                            echo "<p> Who has not repaid his debt </p>";
+                            echo "<p> I've been searching for a trail to follow again </p>";
+                            echo "<p> Take me back to the night we met </p>";
+                            echo "<p> </p>";
+
+                            echo "<p> And then I can tell myself </p>";
+                            echo "<p> What the hell I'm supposed to do </p>";
+                            echo "<p> And then I can tell myself </p>";
+                            echo "<p> Not to ride along with you </p>";
+                            echo "<br/>";
+
+                            echo "<p> I had all and then most of you, some and now none of you </p>";
+                            echo "<p> Take me back to the night we met </p>";
+                            echo "<p> I don't know what I'm supposed to do, haunted by the ghost of you </p>";
+                            echo "<p> Oh, take me back to the night we met </p>";
+                            echo "<br/>";
+
+                            echo "<p> When the night was full of terrors </p>";
+                            echo "<p> And your eyes were filled with tears </p>";
+                            echo "<p> When you had not touched me yet </p>";
+                            echo "<p> Oh, take me back to the night we met </p>";
+                            echo "<br/>";
+
+                            echo "<p> I had all and then most of you, some and now none of you </p>";
+                            echo "<p> Take me back to the night we met </p>";
+                            echo "<p> I don't know what I'm supposed to do, haunted by the ghost of you </p>";
+                            echo "<p> Take me back to the night we met </p>";
+                        ?>
