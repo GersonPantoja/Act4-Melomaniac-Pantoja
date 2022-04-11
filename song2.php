87ccb70d94776375fbd5a3fb786fbdd4937f3844
PantojaGerson <gersonpantoja22.gp@gmail.com>
2022-04-11 10:25:05 +0800

    Song 2 lyrics has been added

--- song2.php
+++ song2.php
@@ -14,7 +14,7 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
             <div class="container px-4">
-                <a class="navbar-brand" href="#page-top"> Add your brand name here </a>
+                <a class="navbar-brand" href="#page-top"> MeloDicted </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
@@ -30,8 +30,8 @@
         <!-- Header-->
         <header class="bg-dark bg-gradient text-white">
             <div class="container px-4 text-center">
-                <h1 class="fw-bolder">Welcome to Melomaniac</h1>
-                <p class="lead">Add a short description here</p>
+                <h1 class="fw-bolder">Welcome to MeloDicted</h1>
+                <p class="lead"><i>Music is an outburst of the soul</i></p>
                 
             </div>
         </header>
@@ -41,9 +41,67 @@
                 <div class="row gx-4 justify-content-center">
                     <div class="col-lg-12 text-center">
                         <!-- Song title and singer-->
-                        <h2 class="text-primary">Song Title</h2>
-                        <p class="lead">Artist name</p>
+                        <h2 class="text-primary">Anyone</h2>
+                        <p class="lead"><b>Justin Bieber</b></p>
                         <!-- Lyrics hear -->
+                        <?php
+
+                            echo "<p> Dance with me under the diamonds </p>";
+                            echo "<p> See me like breath in the cold </p>";
+                            echo "<p> Sleep with me here in the silence </p>";
+                            echo "<p> Come kiss me, silver and gold </p>";
+                            echo "<br/>";
+
+                            echo "<p> You say that I won't lose you, but you
+                                    can't predict the future </p>";
+                            echo "<p> So just hold on like you will never let go </p>";
+                            echo "<p> Yeah, if you ever move on without me </p>";
+                            echo "<p> I need to make sure you know that </p>";
+                            echo "<br/>";
+
+                            echo "<p> You are the only one I'll ever love
+                                    (I gotta tell ya, gotta tell ya) </p>";
+                            echo "<p> Yeah, you, if it's not you, it's not anyone
+                                    (I gotta tell ya, gotta tell ya) </p>";
+                            echo "<p>  Lookin' back on my life, you're the only good I've ever done
+                                    (Ever done) </p>";
+                            echo "<p> Yeah, you, if it's not you, it's not anyone (Anyone), not anyone </p>";
+                            echo "<br/>";
+
+                            echo "<p> Forever's not enough time to (Oh) </p>";
+                            echo "<p> Love you the way that I want (Love you the way that I want) </p>";
+                            echo "<p> Cause' every mornin', I'll find you (Oh) </p>";
+                            echo "<p> I fear the day that I don't </p>";
+                            echo "<br/>";
+
+                             echo "<p> You say that I won't lose you, but you
+                                    can't predict the future </p>";
+                            echo "<p> Cause' certain things are out of our control </p>";
+                            echo "<p> Yeah, if you ever move on without me </p>";
+                            echo "<p> I need to make sure you know that </p>";
+                            echo "<br/>";
+
+                            echo "<p> You are the only one I'll ever love
+                                    (I gotta tell ya, gotta tell ya) </p>";
+                            echo "<p> Yeah, you, if it's not you, it's not anyone
+                                    (I gotta tell ya, gotta tell ya) </p>";
+                            echo "<p>  Lookin' back on my life, you're the only good I've ever done
+                                    (I've ever done) </p>";
+                            echo "<p> Yeah, you, if it's not you, it's not anyone (It's not anyone, not anyone </p>";
+                            echo "<br/>";
+
+                            echo "<p> Oh-oh-oh-oh </p>";
+                            echo "<p> If it's not you, it's not anyone </p>";
+                            echo "<p> oh-oh-oh, yeah, woah</p>";
+                            echo "<br/>";
+
+                            echo "<p> Yeah, you are the only one I'll ever love (I gotta tell ya, gotta tell ya, yeah, oh-oh-oh-oh) </p>";
+                            echo "<p> Yeah, you, if it's not you, it's not anyone (I gotta tell ya, gotta tell you </p>";
+                            echo "<p> Lookin' back on my life, you're the only good I've ever done (Ever done, oh yeah) </p>";
+                            echo "<p> Yeah, you, if it's not you, it's not anyone </p>";
+
+                        ?>
+                        
                     </div>
                 </div>
             </div>
