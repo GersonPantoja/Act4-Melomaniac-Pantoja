93fb79af2d17d14e1851c0b878b3a6c9ff9f6c4e
PantojaGerson <gersonpantoja22.gp@gmail.com>
2022-04-11 10:36:31 +0800

    Song 4 lyrics has been added

--- song4.php
+++ song4.php
@@ -14,7 +14,7 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
             <div class="container px-4">
-                <a class="navbar-brand" href="#page-top"> Add your brand name here </a>
+                <a class="navbar-brand" href="#page-top"> MeloDicted </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
@@ -30,8 +30,8 @@
         <!-- Header-->
         <header class="bg-dark bg-gradient text-white">
             <div class="container px-4 text-center">
-                <h1 class="fw-bolder">Welcome to Melomaniac</h1>
-                <p class="lead">Add a short description here</p>
+                <h1 class="fw-bolder">Welcome to MeloDicted</h1>
+                <p class="lead">Music is an outburtst of the soul</p>
                 
             </div>
         </header>
@@ -41,16 +41,93 @@
                 <div class="row gx-4 justify-content-center">
                     <div class="col-lg-12 text-center">
                         <!-- Song title and singer-->
-                        <h2 class="text-primary">Song Title</h2>
-                        <p class="lead">Artist name</p>
+                        <h2 class="text-primary">Manila</h2>
+                        <p class="lead">Young Cocoa</p>
                         <!-- Lyrics hear -->
+                        <?php
+
+                            echo "<p> Yeah, uh, woah, yeah, ayy </p>";
+                            echo "<br/>"
+
+                            echo "<p> Urban Jungle, steppin' on a puddle </p>";
+                            echo "<p> Mind race ejaculate the thoughts that's in the bubble </p>";
+                            echo "<p> Workin' for the better 'morrow, workin' for the payday</p>";
+                            echo "<p> Rememberin' the things that's in the past that's in the heyday</p>";
+                            echo "<p> Way way, back back, when when, time right </p>";
+                            echo "<p> Sneakers so white white, comin' through the city </p>";
+                            echo "<br/>";
+
+                            echo "<p> Comin' out just for night night </p>";
+                            echo "<p> Vios got modded and all </p>";
+                            echo "<p> And I'm whippin' out my paper on my favorite stall </p>";
+                            echo "<p> Mang nestor, feelin' like a jester </p>";
+                            echo "<p> Swervin' so heavy alcohol go up on tester </p>";
+                            echo "<p> Pourin' out a bottle just to show respect for ones that I love </p>";
+                            echo "<p> It's about a little time I show Manila some love </p>";
+                            echo "<br/>";
+
+                            echo "<p> Manila (you know, you know, you know) yeah, uh, woah, yeah, ayy</p>";
+                            echo "<p> Manila (you know, you know, you know) yeah, woah,  ayy, yeah</p>";
+                            echo "<p> Manila (you know, you know, you know) ayy, yeah, woah";
+                            echo "<p> (You know, you know, you know) </p>";
+
+                            echo "<p> Got a couple hundred in my pocket tryna' to see ya </p>";
+                            echo "<p> Blowin' out my stash, bouta see if I can re-up </p>";
+                            echo "<p> Pullin' up in Katip bouta see if I can see homie </p>";
+                            echo "<p> Got a girl from Alabang and she gon' be on me </p>";
+                            echo "<p> Pullin' up in EDSA, swerve down the skyway </p>";
+                            echo "<p> Ain't no bad vibes 2020 this is my way </p>";
+                            echo "<p> My way or the highway </p>";
+                            echo "<br/>";
+
+                            echo "<p> Pull up in the Angkas </p>";
+                            echo "<p> Don't forget to (skrr skrr) </p>";
+                            echo "<p> I don't fuck with Joyride's 'cause they don't respect the worker </p>";
+                            echo "<p> Diokno in the biker seat got him with a mask and a globe </p>";
+                            echo "<p> And I exercise my right to pay respect to Above </p>";
+                            echo "<p> Filipino kid, just a mix of cultural things </p>";
+                            echo "<p> Showin' love to Manila it's city of dreams </p>";
+                            echo "<br/>";
+
+                            echo "<p> Yeah, slide through with a couple of friends </p>";
+                            echo "<p> Jammin' out so heavy got a chos in my hands </p>";
+                            echo "<p> Grew up in the city call J-K-T </p>";
+                            echo "<p> With the tempe, sauce and a couple of beats </p>";
+                            echo "<p> Flew back to Manila in search of my own </p>";
+                            echo "<p> Pay respect to the city that became my home </p>";
+                            echo "<br/>";
+
+                            echo "<p> Manila (you know, you know, you know) ayy, yeah, woah, yeah </p>";
+                            echo "<p> Manila (you know, you know, you know) uh, yeah, woah, ayy, yeah</p>";
+                            echo "<p> Manila (you know, you know, you know) ayy, yeah, woah";
+                            echo "<p> (You know, you know, you know) </p>";
+
+                            echo "<p> Got a couple hundred in my pocket tryna' to see ya </p>";
+                            echo "<p> Blowin' out my stash, bouta see if I can re-up </p>";
+                            echo "<p> Pullin' up in Katip bouta see if I can see homie </p>";
+                            echo "<p> Got a girl from Alabang and she gon' be on me </p>";
+                            echo "<p> Pullin' up in EDSA, swerve down the skyway </p>";
+                            echo "<p> Ain't no bad vibes 2020 this is my way </p>";
+                            echo "<p> My way or the highway </p>";
+                            echo "<br/>";
+
+                            echo "<p> Got a couple hundred in my pocket tryna' to see ya </p>";
+                            echo "<p> Blowin' out my stash, bouta see if I can re-up </p>";
+                            echo "<p> Pullin' up in Katip bouta see if I can see homie </p>";
+                            echo "<p> Got a girl from Alabang and she gon' be on me </p>";
+                            echo "<p> Pullin' up in EDSA, swerve down the skyway </p>";
+                            echo "<p> Ain't no bad vibes 2020 this is my way </p>";
+                            echo "<p> My way or the highway </p>";
+                            echo "<br/>";
+
+                        ?>
                     </div>
                 </div>
             </div>
         </section>
 
         <footer class="py-3 bg-danger">
-            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. your full name</p></div>
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Gerson O. Pantoja</p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="js/bootstrap.bundle.min.js"></script>
