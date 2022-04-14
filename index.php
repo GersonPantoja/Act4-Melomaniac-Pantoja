be24fbf4717601dcbd2f840aaca18e759ddcddeb
PantojaGerson <gersonpantoja22.gp@gmail.com>
2022-04-14 14:45:20 +0800

    some minor changes

--- index.php
+++ index.php
@@ -7,30 +7,31 @@
         <meta name="author" content="" />
         <title>Melomaniac Activity</title>
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+
       
         <link href="css/styles.css" rel="stylesheet" />
     </head>
     <body id="page-top">
         <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
             <div class="container px-4">
-                <a class="navbar-brand" href="#page-top"> MeloDicted </a>
+                <a class="navbar-brand" href="#page-top">MeloDicted</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
-                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
-                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
-                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
-                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">SONG 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">SONG 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">SONG 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">SONG 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">SONG 5</a></li>
                     </ul>
                 </div>
             </div>
         </nav>
         <!-- Header-->
         <header class="bg-dark bg-gradient text-white">
-            <div class="container px-4 text-center">
-                <h1 class="fw-bolder">Welcome to MeloDicted</h1>
+                <div class="container px-5 text-center">
+                <h1 class="fw-bolder">Welcome To MeloDicted</h1>
                 <p class="lead"><i>Music is an outburst of the soul</i></p>
                 
             </div>
@@ -41,7 +42,7 @@
                 <div class="row gx-4 justify-content-center">
                     <div class="col-lg-12 text-center">
                         <!-- Song title and singer-->
-                        <h2 class="text-primary">Song Titles</h2>
+                        <h2 class="text-success">Song Titles</h2>
                         <!-- <p class="lead">Artist name</p> -->
                         <?php
                             echo "<br/>";
@@ -54,9 +55,9 @@
                     </div>
                 </div>
             </div>
-        </section>
+        </section>  
 
-        <footer class="py-3 bg-danger">
+        <footer class="py-3 bg-success">
             <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Gerson O. Pantoja</p></div>
         </footer>
         <!-- Bootstrap core JS-->
--- song1.php
+++ song1.php
@@ -12,17 +12,17 @@
     </head>
     <body id="page-top">
         <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
             <div class="container px-4">
-                <a class="navbar-brand" href="index.php"> MeloDicted </a>
+                <a class="navbar-brand" href="index.php">MeloDicted</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 1</a></li>
-                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
-                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
-                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
-                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="#"><b>SONG 1</b></a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">SONG 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">SONG 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">SONG 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">SONG 5</a></li>
                     </ul>
                 </div>
             </div>
@@ -41,7 +41,7 @@
                 <div class="row gx-4 justify-content-center">
                     <div class="col-lg-12 text-center">
                         <!-- Song title and singer-->
-                        <h2 class="text-primary">Habang Buhay</h2>
+                        <h2 class="text-success">Habang Buhay</h2>
                         <p class="lead"><b>Zack Tabudlo</b></p>
                         <!-- Lyrics hear -->
                         <?php
@@ -140,7 +140,7 @@
             </div>
         </section>
 
-        <footer class="py-3 bg-danger">
+        <footer class="py-3 bg-success">
             <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Gerson O. Pantoja</p></div>
         </footer>
         <!-- Bootstrap core JS-->
