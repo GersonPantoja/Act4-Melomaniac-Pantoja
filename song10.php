e05bae8a6e1451d993f3f95e836611f98631f963
PantojaGerson <gersonpantoja22.gp@gmail.com>
2022-04-14 22:24:55 +0800

    song10 lyrics has been added

--- song10.php
+++ song10.php
@@ -43,9 +43,70 @@
                 <div class="row gx-4 justify-content-center">
                     <div class="col-lg-12 text-center">
                         <!-- Song title and singer-->
-                        <h2 class="text-success">Song Titles</h2>
-                        <!-- <p class="lead">Artist name</p> -->
+                        <h2 class="text-success">A Thousand Bad Times </h2>
+                        <p class="lead">Post Malone</p> -->
                         <?php
+                            echo "<p> Said you needed a ride, but you wanted my car </p>";
+                            echo "<p> Without that face, girl, you wouldn't get far </p>";
+                            echo "<p> I really like you, despite who you are (Who you are) </p>";
+                            echo "<p> You see me on tv, you know I'm a star </p>";
+                            echo "<p> You say you don't know me, but I know that's false </p>";
+                            echo "<p> I'll pray the price, girl, whatever the cost (What it cost)</p>";
+                            echo "<br/>";
+
+                            echo "<p> You make my life so hard </p>";
+                            echo "<p> But that's what gets me off </p>";
+                            echo "<br/>";
+
+                            echo "<p> I had a thousand bad times, so what's another time to me? (What's that to me) </p>";
+                            echo "<p> You tried to blow my house down, but what's another house to me? (What's that to me?) </p>";
+                            echo "<p> 'Cause I can take anything that you give me (That you give) </p>";
+                            echo "<p> It's gonna take a lot more to kill me, bitch </p>";
+                            echo "<p> So thank you for the grave, I needed me a place to sleep (Place to sleep) </p>";
+                            echo "<p> And I don't wanna meet your mama (No)</p>";
+                            echo "<p> She pro'lly crazier than you (She'll pro'lly crazier than you)</p>";
+                            echo "<p> I'm gonna need some thicker armor (Armor) </p>";
+                            echo "<p> To spend another night with you, (And one and on and on)</p>";
+                            echo "<br/>";
+
+                            echo "<p> Baby, I know just what to do </p>";
+                            echo "<p> Every time you fuck me over, I come back to you </p>";
+                            echo "<p> Baby, I don't wanna know the truth (Know the truth) </p>";
+                            echo "<p> I ignore them when they tell me all the shit you do (Shit you do) </p>";
+                            echo "<p> I always get my heart broke, like I needed the practice </p>";
+                            echo "<p> Foot on my throat 'til my world is collapsing </p>";
+                            echo "<p> Now this what I chose, it's the law of attraction, yeah </p>";
+                            echo "<p> (And on and on and on)</p>";
+                            echo "<br/>";
+
+                            echo "<p> Now you make my life so hard </p>";
+                            echo "<p> But that's what gets me off</p>";
+                            echo "<br/>";
+
+                            echo "<p> I had a thousand bad times, so what's another time to me? (What's that to me) </p>";
+                            echo "<p> You tried to blow my house down, but what's another house to me? (What's that to me?) </p>";
+                            echo "<p> 'Cause I can take anything that you give me (That you give) </p>";
+                            echo "<p> It's gonna take a lot more to kill me, bitch </p>";
+                            echo "<p> So thank you for the grave, I needed me a place to sleep (Place to sleep) </p>";
+                            echo "<br/>";
+
+                            echo "<p> I should get out, but I still want more </p>";
+                            echo "<p> I should get out, what am I waiting for </p>";
+                            echo "<br/>";
+
+                            echo "<p> It's all the same to me </p>";
+                            echo "<p> It's all a game to me </p>";
+                            echo "<p> It's all the same to me </p>";
+                            echo "<p> It's all the same </p>";
+                            echo "<br/>";
+
+                            echo "<p> I had a thousand bad times, so what's another time to me? (What's that to me) </p>";
+                            echo "<p> You tried to blow my house down, but what's another house to me? (What's that to me?) </p>";
+                            echo "<p> 'Cause I can take anything that you give me (That you give) </p>";
+                            echo "<p> It's gonna take a lot more to kill me, bitch </p>";
+                            echo "<p> So thank you for the grave, I needed me a place to sleep (Place to sleep) </p>";
+                            echo "<br/>";
+                            echo "<p> We go there, oh</p>";
 
                         ?>
                     </div>
