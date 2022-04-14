cd46e6ee40c8a2f695675367001e595d1f8fd954
PantojaGerson <gersonpantoja22.gp@gmail.com>
2022-04-14 21:34:30 +0800

    song8 lyrics has been added

--- song8.php
+++ song8.php
@@ -46,6 +46,107 @@
                         <h2 class="text-success"> Come and get Your Love </h2>
                         <p class="lead"> Redbone </p>
                         <?php
+                            
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> </p>";
+
+                            echo "<p> Hail(Hail), what's the matter with your head, yeah </p>";
+                            echo "<p> Hail(Hail), what's the matter with your mind </p>";
+                            echo "<p> And your sign an-a, oh-oh-oh </p>";
+                            echo "<p> Hail(Hail), nothin' the matter with your head </p>";
+                            echo "<p> Baby find it, come on and find it </p>";
+                            echo "<p> Hail, with it, baby, 'cause you're fine </p>";
+                            echo "<p> And you're mine, and you look so divine </p>";
+                            echo "<br/>";
+
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> </p>";
+
+                            echo "<p> Hail(Hail), what's the matter with you feel right </p>";
+                            echo "<p> Don't you feel right, baby </p>";
+                            echo "<p> Hail, oh yeah, get it from the main vine, all right </p>";
+                            echo "<p> I said-a find it, find it, go on and love it if you like it, yeah </p>";
+                            echo "<p> Hail(Hail), it's your business if you want some, take some </p>";
+                            echo "<p>Get it together, baby </p>";
+                            echo "<br/>";
+
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> </p>";
+
+                            echo "<p> Come and get your love, come and get your love</p>";
+                            echo "<p> Come and get your love now </p>";
+                            echo "<p> Come and get your love, come and get your love </p>";
+                            echo "<p> Come and get your love now </p>";
+                            echo "<p> Come and get your love, come and get your love </p>";
+                            echo "<p> Come and get your love now </p>";
+                            echo "<p> Come and get your love, come and get your love </p>";
+                            echo "<p> Come and get your love now </p>";
+                            echo "<br/>";
+
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<br/>";
+
+                            echo "<p> Hail(Hail), what's the matter with you feel right </p>";
+                            echo "<p> Don't you feel right baby </p>";
+                            echo "<p> Hail, oh yeah, get it from the main vine, alright </p>";
+                            echo "<br/>";
+
+                            echo "<p> La, la, la, la, la, la , la, la, la </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> La, la, la, la, la, la , la, la, la, wohooo </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> La, la, la, la, la, la , la, la, la </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> La, na, na, na, na, na, da, boom </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> La, da, boom,boom,boom, ta, daba, boom, boom </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> La, la, la, la, la, la , la, la, la, wohooo </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> La, la, la, la, la, la , la, la, la </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> La, la, la, la, la, la , la, la, la </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> La, la, la, la, la, la , la, la, la </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> La, la, la, la, la, la , la, la, la, woohoo </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> La, la, la, la, la, la , la, la, la </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> La, la, la, la, la, la , la, la, la </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> La, la, la, la, la, la , la, la, la </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> La, la, la, la, la, la , la, la, la, woohoo </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> La, la, la, la, la, la , la, la, la </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> La, la, la, la, la, la , la, la, la </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> La, da boom, boom, boom, ta, daba, boom, boom </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> La, da boom, boom, boom, ta, daba, boom, boom </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> La, da boom, boom, boom, ta, daba, boom, boom </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> La, da boom, boom, boom, ta, daba, boom, boom </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> La, la, la, la, la, la , la, la, la </p>";
+                            echo "<p> Come and get your love </p>";
+                            echo "<p> La, la, la, la, la, la , la, la, la, wohooo </p>";
+                            echo "<p> Come and  get your love </p>";
 
                         ?>
                     </div>
